72a200447114d52ddfd29b7a53804ade965da317
kkminseok <aaaa727@khu.ac.kr>
2025-09-13 21:27:35 +0900

    [Java-Test] Stream 4

---
+++ book/modern-java-in-action/example/src/test/java/Chapter4.java
@@ -0,0 +1,106 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class Chapter4 {
+
+
+    List<Dish> menu = Arrays.asList(
+            new Dish("pork", false, 800, Dish.Type.MEAT),
+            new Dish("beef", false, 700, Dish.Type.MEAT),
+            new Dish("chicken", false, 400, Dish.Type.MEAT),
+            new Dish("french fries", true, 530, Dish.Type.OTHER),
+            new Dish("rice", true, 350, Dish.Type.OTHER),
+            new Dish("season fruit", true, 120, Dish.Type.OTHER),
+            new Dish("pizza", true, 550, Dish.Type.OTHER),
+            new Dish("prawns", false, 400, Dish.Type.FISH),
+            new Dish("salmon", false, 400, Dish.Type.FISH)
+    );
+
+
+    @Test
+    void getThreeHighCaloricDishNames() {
+        List<String> threeHighCaloricDishNames =
+                menu.stream()
+                        .filter(dish -> dish.getCalories() > 300)
+                        .map(Dish::getName)
+                        .limit(3)
+                        .collect(toList());
+        threeHighCaloricDishNames.forEach(System.out::println);
+    }
+
+    @Test
+    void quiz1() {
+        List<String> highCaloricDishes = new ArrayList<>();
+        Iterator<Dish> iterator = menu.iterator();
+        while (iterator.hasNext()) {
+            Dish dish = iterator.next();
+            if(dish.getCalories() > 300) {
+                highCaloricDishes.add(dish.getName());
+            }
+        }
+
+        // 개선
+
+        menu.stream()
+                .filter(dish -> dish.getCalories()> 300)
+                .map(Dish::getName)
+                .collect(toList());
+    }
+
+    @Test
+    void quiz2() {
+        long count = menu
+                .stream()
+                .filter(d -> d.getCalories() > 300)
+                .distinct()
+                .limit(3)
+                .count();
+        // count는 최종연산
+    }
+
+
+}
+
+class Dish {
+    private final String name;
+    private final boolean vegetarian;
+    private final int calories;
+    private final Type type;
+
+    Dish(String name, boolean vegetarian, int calories, Type type) {
+        this.name = name;
+        this.vegetarian = vegetarian;
+        this.calories = calories;
+        this.type = type;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public boolean isVegetarian(){
+        return vegetarian;
+    }
+
+    public int getCalories(){
+        return calories;
+    }
+
+    public Type getType(){
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public enum Type {MEAT, FISH, OTHER}
+}
