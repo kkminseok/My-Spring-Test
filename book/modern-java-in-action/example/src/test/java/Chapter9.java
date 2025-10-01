e0f8dba8518f829d781a61a02029a897e5627bb1
kkminseok <aaaa727@khu.ac.kr>
2025-10-01 09:49:05 +0900

    [Java-Test] Modern java in action 9 누락내용 추

--- book/modern-java-in-action/example/src/test/java/Chapter9.java
+++ book/modern-java-in-action/example/src/test/java/Chapter9.java
@@ -34,6 +34,33 @@ public class Chapter9 {
         //의도적으로 람다 에러 발생
         List<Point> points = Arrays.asList(new Point(1,2), null);
         points.stream().map(p -> p.getX() + p.getY()).forEach(System.out::println);
-
     }
+
+    @Test
+    @DisplayName("전략 패턴 개선")
+    void strategyPattern() {
+        Validator numericValidator = new Validator((s) -> s.matches("\\d+"));
+        Validator lowerCaseValidator = new Validator((s) -> s.matches("[a-z]+"));
+
+        System.out.println(numericValidator.validate("1234")); // true
+        System.out.println(numericValidator.validate("1234abc")); // false
+    }
+
+
+
+
+    interface ValidationStrategy {
+        boolean execute(String s);
+    }
+
+    class Validator {
+        private final ValidationStrategy strategy;
+        public Validator(ValidationStrategy strategy) {
+            this.strategy = strategy;
+        }
+        public boolean validate(String s) {
+            return strategy.execute(s);
+        }
+    }
+
 }
